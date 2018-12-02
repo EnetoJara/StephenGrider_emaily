48344d84c011c5540f53762aa4b27774fb23cc57
Seb Ringrose <sebringrose@gmail.com>
2018-12-02 17:48:08 +0000

    added react client to project

--- server/services/passport.js
+++ server/services/passport.js
@@ -24,7 +24,8 @@ passport.use(
       // params sent in header
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       // check if profile ID (googleID) is already assigned to a user.
